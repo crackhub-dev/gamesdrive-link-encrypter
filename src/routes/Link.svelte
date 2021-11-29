88554e6d4e9faa8ecc4046461cae19d5ebc16c46
crackhub213 <64047921+crackhub-dev@users.noreply.github.com>
2021-11-29 21:16:12 +0100

    no more dot if no uploader name is added

--- src/routes/Link.svelte
+++ src/routes/Link.svelte
@@ -1,24 +1,25 @@
 <script>
 	import { Base64 } from 'js-base64';
 	export let params = {};
-    	let data = params.d;
+    let data = params.d;
 	let url = data.split(".")[0];
 	let uploader = data.split(".")[1];
 	let decrypted_url = Base64.decode(url);
-	let decrypted_uploader = Base64.decode(uploader);							
-	
 </script>
 <head>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
   </head>
 <main>
-	{#if uploader.length > 0}
-		<h3 style="text-align: center;">Link Provided by <b>{decrypted_uploader}</b></h3>
+	<div class="container d-flex justify-content-center ">
+	<div class="container">
+	{#if data.includes(".") && uploader.length > 0 }
+		<h3>Link Provided by <b>{Base64.decode(uploader)}</b></h3>
+		{:else}
+		<h3>Link Provided by Gamesdrive</h3>
 	{/if}
-	{#if uploader.length == 0}
-		<h3 style="text-align: center;">Link Provided by Gamesdrive</h3>
-	{/if}
-	<a href="{decrypted_url}" ><button style="cursor: pointer;" class="btn btn-primary btn-lg">Download</button></a>
+	<a href="{decrypted_url}" target="_blank"><button class="btn btn-primary btn-lg"><i class="bi bi-download"></i> Download</button></a>
+</div>
+</div>
 </main>
